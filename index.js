57ed3802e6217eea20bc6310644eb6d37115d098
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-24 13:33:34 -0500

    changement de l'image de base

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ let diceImg = document.getElementById("diceImg");
 //Fonction permettant les valeurs aleatoire de 1 a 6
 function rollDice() {
 	diceValue = Math.floor(Math.random() * 6) + 1;
-	console.log(diceValue);
-
-	console.log(`images/dice${diceValue}.png`);
 
 	diceImg.src = `images/dice${diceValue}.png`;
 }
