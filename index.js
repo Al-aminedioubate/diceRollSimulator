b56a14c6a6a1dcd1c0031ab0c6e0a47d53922385
Lamine DIOUBATE <domanilamine96@gmail.com>
2026-01-24 13:16:08 -0500

    generation de variable affichage de dé en fonction de la valeur terminer

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 //**Initialisation et declaration de nos variables */
 let btnDice = document.getElementById("diceBtn");
+let diceImg = document.getElementById("diceImg");
 
 //Fonction permettant les valeurs aleatoire de 1 a 6
-function rollDice(diceValue) {
+function rollDice() {
 	diceValue = Math.floor(Math.random() * 6) + 1;
 	console.log(diceValue);
+
+	console.log(`images/dice${diceValue}.png`);
+
+	diceImg.src = `images/dice${diceValue}.png`;
 }
 
 //Creons notre evenement
